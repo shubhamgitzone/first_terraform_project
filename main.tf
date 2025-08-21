cd0e84031f1c8f162fd8249868001fb1a147977c
EC2 Default User <ec2-user@ip-172-31-34-64.eu-north-1.compute.internal>
2025-08-21 14:24:14 +0000

    tried createing spotEc2

--- main.tf
+++ main.tf
@@ -13,11 +13,49 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "app_server"{
-  ami = "ami-0c4fc5dcabc9df21d"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
-  tags={
-    name = "Terraform_demo"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+data "aws_ami" "spotEc2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami = data.aws_ami.spotEc2.id
+  instance_type = "t4g.nano"
+   
+  tags = {
+    Name = "test-spot"
   }
 }
