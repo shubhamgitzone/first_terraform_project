0d5a4482088a8b796c5cca90086f4282ea1f9f55
EC2 Default User <ec2-user@ip-172-31-34-64.eu-north-1.compute.internal>
2025-08-22 13:32:16 +0000

    created spot instance

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "~> 4.16"
+        version = ">= 2.60.0"
     }
   }
 
@@ -57,5 +57,62 @@ resource "aws_instance" "web2" {
    
   tags = {
     Name = "test-spot"
+  }
+}
+
+# =========================Day 2 ==============================
+
+# Creating security group that allows public access to the ec2 instance 
+# Attach this group to cerated ec2 amchine to acheive public access
+
+resource "aws_security_group" "public_http" {
+  name        = "public-http"
+  description = "Allow HTTP access from anywhere"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Creating ec2 spot instance with attaching security group
+# to create spot instance, aws version must be greater than 2.60.0 
+# although spot_option instance_intrupption_behavior was not working as expected
+# the index.html file location must be specfice while providing user data
+# to view the index.html, copy and paste the public ip manually
+
+resource "aws_instance" "spot_hello_html" {
+  ami           = "ami-0c4fc5dcabc9df21d" 
+  instance_type = "t3.micro"
+  
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "terminate"
+    }
+  }
+
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo "<html><body>Hello World</body></html>" > /var/www/html/index.html
+  EOF
+  
+  vpc_security_group_ids = [aws_security_group.public_http.id]
+
+  tags = {
+    Name = "SpotHelloWorldHTML"
   }
 }
